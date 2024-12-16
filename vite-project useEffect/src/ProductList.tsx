0b9dab775458960279b3756e65db8998c2a6b731
Dev-Pratham <pratham.chaurasia31@gmail.com>
2024-12-16 20:22:40 +0530

    implemented useEffect Hook

---
+++ vite-project useEffect/src/ProductList.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+interface Props {
+  category: string;
+}
+
+const ProductList = ({ category }: Props) => {
+  //  just below code is for demo purposes
+  //   const [products, setProducts] = useState<string[]>([]);
+  //using the effect hook to call the server to fetch products
+
+  useEffect(() => {
+    console.log("Fetching products", category);
+    //below code is just for demo
+    // setProducts(["clothing", "household"]);
+  }, [category]);
+
+  return <div>Products</div>;
+};
+
+export default ProductList;
